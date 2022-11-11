5b73702ce0103b5079b01dec66c8cd9803cd7b11
parduetyler <parduetyler@gmail.com>
2022-11-10 23:57:16 -0500

    initial commit

--- Assets/Scripts/bossBehavior.cs
+++ Assets/Scripts/bossBehavior.cs
@@ -22,6 +22,7 @@ public class bossBehavior : MonoBehaviour
             {
                 Instantiate(trophy, transform.localPosition, Quaternion.identity);
                 trophySpawned = true;
+                //test
             }
 
         }
